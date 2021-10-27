081049c55aa3a4bf10f779f6a550de8f0fcc452a
KarenVeron <89805237+KarenVeron@users.noreply.github.com>
2021-10-27 16:25:21 -0300

    Update ReglaAsignacion.java
    Agrego

--- src/main/java/com/arquitectura/web/sistfcliente/entity/ReglaAsignacion.java
+++ src/main/java/com/arquitectura/web/sistfcliente/entity/ReglaAsignacion.java
@@ -1,52 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package com.arquitectura.web.sistfcliente.entity;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 /**
  *
  * @author Karen
  */
+@Entity(name="ReglaAsigancion")
+@Table(name="regla_asignacion")
 public class ReglaAsignacion {
 
-private Integer id;         
-private Integer limiteInf;
-private Integer limiteSup;    
-private Integer montoEqPunto;
+    @Id
+    @Column (name="id")
+    @Basic (optional = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; 
+ 
+    @Column (name="limite_inf")
+    @Basic (optional = false)
+    private Integer limiteInf;
 
- public ReglaAsignacion() {
+    @Column (name="limite_sup")
+    @Basic (optional = false)
+    private Integer limiteSup; 
+ 
+    @Column (name="monto_eq_punto")
+    @Basic (optional = false)
+    private Integer montoEqPunto;
+
+    public ReglaAsignacion() {
     }
 
     public ReglaAsignacion(Integer id) {
         this.id = id;
     }
 
-public Integer getId() {
+    public ReglaAsignacion(Integer id, Integer limiteInf, Integer limiteSup, Integer montoEqPunto) {
+        this.id = id;
+        this.limiteInf = limiteInf;
+        this.limiteSup = limiteSup;
+        this.montoEqPunto = montoEqPunto;
+    }
+
+    public Integer getId() {
         return id;
     }
-public Integer getLimiteInf() {
+
+    public Integer getLimiteInf() {
         return limiteInf;
     }
-public Integer getLimiteSup() {
+
+    public Integer getLimiteSup() {
         return limiteSup;
     }
-public Integer getMontoEqPunto() {
+
+    public Integer getMontoEqPunto() {
         return montoEqPunto;
     }
-public void setId(Integer id) {
+
+    public void setId(Integer id) {
         this.id = id;
     }
-public void setLimiteInf(Integer limiteInf) {
+
+    public void setLimiteInf(Integer limiteInf) {
         this.limiteInf = limiteInf;
     }
-public void setLimiteSup(Integer limiteSup) {
+
+    public void setLimiteSup(Integer limiteSup) {
         this.limiteSup = limiteSup;
     }
-public void setMontoEqPunto(Integer montoEqPunto) {
+
+    public void setMontoEqPunto(Integer montoEqPunto) {
         this.montoEqPunto = montoEqPunto;
+    }
+
+    @Override
+    public String toString() {
+        return "ReglaAsignacion{"+"id= '"+id+"', limiteInf= '"+limiteInf+"',"
+                + " limiteSup= '"+limiteSup+"', montoEqPunto= '"+montoEqPunto+"'}";
     }
 }
 
