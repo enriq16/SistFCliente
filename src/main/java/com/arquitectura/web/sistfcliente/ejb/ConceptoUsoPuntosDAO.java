d9d87281953ae3ac0e7211934ffa703a5554e004
enriq_16 <enriq16@fpuna.edu.py>
2021-12-01 22:09:30 -0300

    DAO para persistencia de Concepto Uso de Puntos

---
+++ src/main/java/com/arquitectura/web/sistfcliente/ejb/ConceptoUsoPuntosDAO.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arquitectura.web.sistfcliente.ejb;
+
+import com.arquitectura.web.sistfcliente.entity.ConceptoUsoPuntos;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Stateless
+public class ConceptoUsoPuntosDAO {
+    @PersistenceContext(unitName = "hibernate_jpa_pu")
+    private EntityManager em;
+    
+        public ConceptoUsoPuntos findById(Integer id){
+        ConceptoUsoPuntos c = em.find(ConceptoUsoPuntos.class, id);
+        if(c == null){
+            throw new EntityNotFoundException("No existe Concepto de uso de puntos con ID: "+id);
+        }
+        return c;
+    }
+    
+    public void agregar(ConceptoUsoPuntos concepto){        
+        em.persist(concepto);                
+        System.out.println("ConceptoUsoPuntos: "+concepto.toString());        
+    }
+    
+    public void update(ConceptoUsoPuntos concepto){
+        ConceptoUsoPuntos c = em.find(ConceptoUsoPuntos.class, concepto.getId());
+                        
+        c.setConcepto(concepto.getConcepto());
+        c.setPuntosRequeridos(concepto.getPuntosRequeridos());        
+        
+        em.persist(c);
+    }
+
+    
+        public List<ConceptoUsoPuntos> listar(){
+        Query q = em.createQuery("select c from ConceptoUsoPuntos c order by c.id ASC");
+        return (List<ConceptoUsoPuntos>) q.getResultList();
+    }
+}
