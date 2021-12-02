52a9c50ff2d3e75782818f2ae95c599f371ab79b
enriq_16 <enriq16@fpuna.edu.py>
2021-12-01 22:11:38 -0300

    Anotaciones para JPA de Concepto Uso Puntos

--- src/main/java/com/arquitectura/web/sistfcliente/entity/ConceptoUsoPuntos.java
+++ src/main/java/com/arquitectura/web/sistfcliente/entity/ConceptoUsoPuntos.java
@@ -1,10 +1,36 @@
 
 package com.arquitectura.web.sistfcliente.entity;
 
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
-public class ConceptoUsoPuntos {
+@Entity(name="ConceptoUsoPuntos")//como referirse a la clase en el hql(query de jpa)
+@Table(name ="concepto_uso")//nombre de la tabla en la bd
+public class ConceptoUsoPuntos implements Serializable {
+    @Id
+    @Basic(optional = false )
+    @Column(name="id")    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+    
+    @Basic(optional=false, fetch=FetchType.EAGER)    
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "descripcion_concepto")        
     String  concepto;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "puntos_requeridos")
     Integer puntosRequeridos;
 
     public ConceptoUsoPuntos() {
